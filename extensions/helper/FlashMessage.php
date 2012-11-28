f61f766c7997a5aab7f10429c6b42ce7ec5aa921
Josey Morton <me@joseymorton.com>
2012-11-28 13:28:23 -0600

    fixing undefined index 'atts' error.

--- extensions/helper/FlashMessage.php
+++ extensions/helper/FlashMessage.php
@@ -54,7 +54,8 @@ class FlashMessage extends \lithium\template\Helper {
 		$output = '';
 		$type = array($options['type'] => $options['template']);
 		$flash = $storage::read($key);
-		
+		$flash['atts'] = !empty($flash['atts']) ? $flash['atts'] : array();
+
 		if (!empty($flash)) {
 			$data = $options['data'] + array('message' => $flash['message']) + $flash['atts'];
 			$storage::clear($key);
